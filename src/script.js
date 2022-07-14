87f7dfd753bc993c933655b320c81ad0c2e3c8c6
Matteo Gratton <icona79@gmail.com>
2022-07-14 10:53:44 +0100

    handle gradients and multiple fills

--- src/script.js
+++ src/script.js
@@ -4,11 +4,22 @@ var selectedItems = document.selectedLayers.layers;
 
 export default function () {
     let colors = [];
+    let fills = [];
+    let multiFills = [];
     if (selectedItems.length === 2) {
         selectedItems.forEach((layer) => {
             if (layer.type === "Shape" || layer.type === "ShapePath") {
-                colors.push(layer.style.fills[0].color);
+                fills.push(layer.style.fills);
+                if (layer.style.fills[0].fillType === "Gradient") {
+                    colors.push(layer.style.fills[0].gradient.stops[0].color);
+                } else {
+                    colors.push(layer.style.fills[0].color);
+                }
+                if (layer.style.fills.length > 1) {
+                    multiFills.push(layer.style.fills);
+                }
             } else if (layer.type === "Text") {
+                console.log(layer.style.textColor);
                 colors.push(layer.style.textColor);
             }
         });
@@ -16,11 +27,29 @@ export default function () {
         let counter = 1;
         selectedItems.forEach((layer) => {
             if (layer.type === "Shape" || layer.type === "ShapePath") {
-                layer.style.fills[0].color = colors[counter];
+                if (multiFills[counter] !== undefined) {
+                    layer.style.fills = multiFills[counter];
+                } else if (fills[counter] !== undefined) {
+                    layer.style.fills = fills[counter];
+                } else {
+                    console.log("Apply here:" + colors[counter]);
+                    layer.style.fills = [];
+                    console.log(layer.style.fills);
+                    layer.style.fills = [
+                        {
+                            fillType: "Color",
+                            color: colors[counter],
+                        },
+                    ];
+
+                    console.log(layer.style.fills);
+                }
             } else if (layer.type === "Text") {
                 layer.style.textColor = colors[counter];
             }
             counter--;
         });
+        fills = [];
+        colors = [];
     }
 }
